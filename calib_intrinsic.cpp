49649d4167a4354b92e40941e9b53ecdad4dd83a
Alessandro Torresani <alessandro@siel.it>
2020-03-26 18:25:11 +0100

    Support for OpenCV4, docker images, c++17 filesystem to read images

--- calib_intrinsic.cpp
+++ calib_intrinsic.cpp
@@ -1,49 +1,47 @@
+#include "popt_pp.h"
 #include <opencv2/core/core.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
 #include <iostream>
-#include "popt_pp.h"
 #include <sys/stat.h>
+#include <experimental/filesystem>
 
 using namespace std;
 using namespace cv;
+namespace fs = std::experimental::filesystem;
 
 vector< vector< Point3f > > object_points;
 vector< vector< Point2f > > image_points;
 vector< Point2f > corners;
 vector< vector< Point2f > > left_img_points;
-
 Mat img, gray;
 Size im_size;
 
-bool doesExist (const std::string& name) {
-  struct stat buffer;   
-  return (stat (name.c_str(), &buffer) == 0); 
+void getImgFilepaths(string _path, string _filetype, vector<string>& _out){
+  for (const auto & entry : fs::directory_iterator(_path)){
+    string filetype = entry.path().extension().u8string();
+    if (filetype.size() > 0 && filetype.substr(1) == _filetype)                 // filetype.substr(1): .png to png
+      _out.emplace_back(entry.path().u8string());
+  }
 }
 
-void setup_calibration(int board_width, int board_height, int num_imgs, 
-                       float square_size, char* imgs_directory, char* imgs_filename,
-                       char* extension) {
+void setup_calibration(int board_width, int board_height, float square_size, const vector<string> imgFilepaths) {
   Size board_size = Size(board_width, board_height);
   int board_n = board_width * board_height;
 
-  for (int k = 1; k <= num_imgs; k++) {
-    char img_file[100];
-    sprintf(img_file, "%s%s%d.%s", imgs_directory, imgs_filename, k, extension);
-    if(!doesExist(img_file))
-      continue;
-    img = imread(img_file, CV_LOAD_IMAGE_COLOR);
-    cv::cvtColor(img, gray, CV_BGR2GRAY);
+  for(string imgPath : imgFilepaths) {
+    img = imread(imgPath, IMREAD_COLOR);
+    cv::cvtColor(img, gray, COLOR_BGR2GRAY);
 
     bool found = false;
     found = cv::findChessboardCorners(img, board_size, corners,
-                                      CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FILTER_QUADS);
+                                      CALIB_CB_ADAPTIVE_THRESH | CALIB_CB_FILTER_QUADS);
     if (found)
     {
       cornerSubPix(gray, corners, cv::Size(5, 5), cv::Size(-1, -1),
-                   TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 30, 0.1));
+                   TermCriteria(TermCriteria::Type::EPS | TermCriteria::Type::MAX_ITER, 30, 0.1));
       drawChessboardCorners(gray, board_size, corners, found);
     }
     
@@ -53,7 +51,7 @@ void setup_calibration(int board_width, int board_height, int num_imgs,
         obj.push_back(Point3f((float)j * square_size, (float)i * square_size, 0));
 
     if (found) {
-      cout << k << ". Found corners!" << endl;
+      cout << imgPath << ". Found corners!" << endl;
       image_points.push_back(corners);
       object_points.push_back(obj);
     }
@@ -73,7 +71,7 @@ double computeReprojectionErrors(const vector< vector< Point3f > >& objectPoints
   for (i = 0; i < (int)objectPoints.size(); ++i) {
     projectPoints(Mat(objectPoints[i]), rvecs[i], tvecs[i], cameraMatrix,
                   distCoeffs, imagePoints2);
-    err = norm(Mat(imagePoints[i]), Mat(imagePoints2), CV_L2);
+    err = norm(Mat(imagePoints[i]), Mat(imagePoints2), NORM_L2);
     int n = (int)objectPoints[i].size();
     perViewErrors[i] = (float) std::sqrt(err*err/n);
     totalErr += err*err;
@@ -84,20 +82,17 @@ double computeReprojectionErrors(const vector< vector< Point3f > >& objectPoints
 
 int main(int argc, char const **argv)
 {
-  int board_width, board_height, num_imgs;
+  int board_width, board_height;
   float square_size;
   char* imgs_directory;
-  char* imgs_filename;
   char* out_file;
   char* extension;
 
   static struct poptOption options[] = {
     { "board_width",'w',POPT_ARG_INT,&board_width,0,"Checkerboard width","NUM" },
     { "board_height",'h',POPT_ARG_INT,&board_height,0,"Checkerboard height","NUM" },
-    { "num_imgs",'n',POPT_ARG_INT,&num_imgs,0,"Number of checkerboard images","NUM" },
     { "square_size",'s',POPT_ARG_FLOAT,&square_size,0,"Size of checkerboard square","NUM" },
     { "imgs_directory",'d',POPT_ARG_STRING,&imgs_directory,0,"Directory containing images","STR" },
-    { "imgs_filename",'i',POPT_ARG_STRING,&imgs_filename,0,"Image filename","STR" },
     { "extension",'e',POPT_ARG_STRING,&extension,0,"Image extension","STR" },
     { "out_file",'o',POPT_ARG_STRING,&out_file,0,"Output calibration filename (YML)","STR" },
     POPT_AUTOHELP
@@ -108,16 +103,19 @@ int main(int argc, char const **argv)
   int c;
   while((c = popt.getNextOpt()) >= 0) {}
 
-  setup_calibration(board_width, board_height, num_imgs, square_size,
-                   imgs_directory, imgs_filename, extension);
+  // Load the image filepaths
+  vector<string> imgFilepaths;
+  getImgFilepaths(imgs_directory, extension, imgFilepaths);
+
+  setup_calibration(board_width, board_height, square_size, imgFilepaths);
 
   printf("Starting Calibration\n");
   Mat K;
   Mat D;
   vector< Mat > rvecs, tvecs;
   int flag = 0;
-  flag |= CV_CALIB_FIX_K4;
-  flag |= CV_CALIB_FIX_K5;
+  flag |= CALIB_FIX_K4;
+  flag |= CALIB_FIX_K5;
   calibrateCamera(object_points, image_points, img.size(), K, D, rvecs, tvecs, flag);
 
   cout << "Calibration error: " << computeReprojectionErrors(object_points, image_points, rvecs, tvecs, K, D) << endl;
